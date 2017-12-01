6473cda51f419acfa21898f61ed2c8f220d7e1a0
Fredrik Hansson <gredrik@gmail.com>
2017-12-01 17:37:39 +0100

    Formatting

--- day1/app.js
+++ day1/app.js
@@ -1,7 +1,7 @@
 const day1 = require('./day1')
-const fs = require('fs')
+const fs = require( 'fs' )
 
-const input = fs.readFileSync(__dirname + '/input.txt').toString()
+const input = fs.readFileSync( __dirname + '/input.txt' ).toString()
 
-console.log(day1.inverseCaptchaA(input))
-console.log(day1.inverseCaptchaB(input))
+console.log( day1.inverseCaptchaA( input ) )
+console.log( day1.inverseCaptchaB( input ) )
