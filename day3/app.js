20ba2dbbbf2282ade94d95f189cb504cf7b5fd4a
Fredrik Hansson <gredrik@gmail.com>
2017-12-13 23:15:27 +0100

    A tad smaller

--- day3/app.js
+++ day3/app.js
@@ -1,48 +1,18 @@
-function* floors() {
-  let 
-    i = 1, 
-    no = 0, 
-    prev = 0
-  while ( true ) {
-    let square = i * i
-    yield { no: no, from: prev, to: square }
-    prev = square
-    i += 2
-    no++
-  }
+const getLevel = n => {
+  const nextOdd = x => x + ( x % 2 ? 2 : 1 )
+  const isEven = x => x % 2 === 0
+  const sqrt = Math.sqrt( n )
+  let i = Math.trunc( sqrt )
+  if ( sqrt - i > 0 || isEven( i ) ) i = nextOdd( i )
+
+  return { level: Math.trunc( i / 2 ), stepsToCorner: i * i - n } 
 }
 
-function* oscillate( n ) {
-  let 
-    direction = -1,
-    i = n
-  while ( true ) {
-    yield i
-    i += direction
-    if ( direction === -1 && i === 0 || direction === 1 && i === n ) direction *= -1
-  }
+const stepsToZero = ( range, stepsToCorner ) => Math.abs( stepsToCorner % ( range * 2 ) - range )
+
+const distance = ( n ) => {
+  const { level, stepsToCorner } = getLevel( n )
+  return level + stepsToZero( level, stepsToCorner )
 }
 
-const floor = ( squareNo ) => {
-  let 
-    fn = floors(),
-    f = 0
-  while ( squareNo > ( f = fn.next().value ).to ) ;
-  return f
-}
-
-const steps = ( steps, limit ) => {
-  let osc = oscillate( limit )
-  for ( let i = 0; i < steps; i++ ) osc.next()
-  return osc.next().value
-}
-
-const manhattanDistance = ( squareNo ) => {
-  let 
-    { no, from } = floor( squareNo ),
-    floorSteps = squareNo - from
-  return no + steps( floorSteps, no )
-}
-
-console.log( manhattanDistance( 347991 ) )
-
+console.log( distance( 347991 ) )
